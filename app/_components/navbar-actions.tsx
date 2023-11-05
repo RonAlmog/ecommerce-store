71b19f544ff7d5e03f65e38c8c9fca90c6806b4d
Ron Almog <ron8132@gmail.com>
2023-11-05 18:01:21 -0500

    add shadcn, buttons

---
+++ app/_components/navbar-actions.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const NavbarActions = (props: Props) => {
+  return <div className="ml-auto flex items-center gap-x-4"></div>;
+};
+
+export default NavbarActions;
--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Container from "./ui/container";
 import MainNav from "./main-nav";
 import getCategories from "@/actions/get-categories";
+import NavbarActions from "./navbar-actions";
 
 type Props = {};
 
@@ -16,6 +17,7 @@ const Navbar = async (props: Props) => {
             <p className="font-bold text-xl">Store</p>
           </Link>
           <MainNav data={categories} />
+          <NavbarActions />
         </div>
       </Container>
     </div>
