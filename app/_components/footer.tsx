5566e8b0ea6e76fcaa7587dda244a340bde01257
Ron Almog <ron8132@gmail.com>
2023-11-05 09:34:51 -0500

    add main-nav, nav-bar

---
+++ app/_components/footer.tsx
@@ -0,0 +1,15 @@
+type Props = {};
+
+const Footer = (props: Props) => {
+  return (
+    <div className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          &copy; 2023 MyBestStore.com inc. All rights reserved somewhere.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
