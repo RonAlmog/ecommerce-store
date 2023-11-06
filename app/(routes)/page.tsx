ebeb7006e0f918546da765de48df3237f367f5e6
Ron Almog <ron8132@gmail.com>
2023-11-05 21:05:36 -0500

    add billboard to main page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,11 +1,17 @@
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
 import Container from "@/components/container";
 
-type Props = {};
+// no cach!
+export const revalidate = 0;
 
-const HomePage = (props: Props) => {
+const HomePage = async () => {
+  const billboard = await getBillboard("0e276fd3-527a-4a9c-98af-8ad30a81acdb");
   return (
     <Container>
-      <div className="space-y-10 pb-10">xx</div>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
     </Container>
   );
 };
