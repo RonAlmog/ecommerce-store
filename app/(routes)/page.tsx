b5cf449c3c49bc22b7399ba4ef8cc3ba6139570a
Ron Almog <ron8132@gmail.com>
2023-11-05 22:41:12 -0500

    add products to home page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,4 +1,5 @@
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
 import Billboard from "@/components/billboard";
 import Container from "@/components/container";
 
@@ -6,11 +7,15 @@ import Container from "@/components/container";
 export const revalidate = 0;
 
 const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
   const billboard = await getBillboard("0e276fd3-527a-4a9c-98af-8ad30a81acdb");
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList />
       </div>
     </Container>
   );
