bd6d69430cecc6de2fd1d3ae403e475a4ca9ddf8
Ron Almog <ron8132@gmail.com>
2023-11-07 13:23:14 -0500

    add filter

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -1,4 +1,10 @@
+import getCategory from "@/actions/get-category";
+import getColors from "@/actions/get-colors";
 import getProducts from "@/actions/get-products";
+import getSizes from "@/actions/get-sizes";
+import Billboard from "@/components/billboard";
+import Container from "@/components/container";
+import Filter from "./_components/filter";
 
 export const revalidate = 0;
 
@@ -17,8 +23,25 @@ const CategoryPage = async ({ params, searchParams }: CategoryPageProps) => {
     colorId: searchParams.colorId,
     sizeId: searchParams.sizeId,
   });
+  const colors = await getColors();
+  const sizes = await getSizes();
+  const category = await getCategory(params.categoryId);
 
-  return <div>CategoryPage</div>;
+  return (
+    <div className="bg-white">
+      <Container>
+        <Billboard data={category.billboard} />
+        <div className="px-4 sm:px-6 lg:px-8 pb-24">
+          <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+            {/* add mobile filters */}
+            <div className="hidden lg:block">
+              <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default CategoryPage;
