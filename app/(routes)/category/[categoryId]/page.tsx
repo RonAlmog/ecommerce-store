0a7a865d32ae4079cf69c9e45e1600fa95ab7465
Ron Almog <ron8132@gmail.com>
2023-11-07 13:39:38 -0500

    add products to category page

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -5,6 +5,8 @@ import getSizes from "@/actions/get-sizes";
 import Billboard from "@/components/billboard";
 import Container from "@/components/container";
 import Filter from "./_components/filter";
+import NoResults from "@/components/no-results";
+import ProductCard from "@/components/product-card";
 
 export const revalidate = 0;
 
@@ -36,6 +38,15 @@ const CategoryPage = async ({ params, searchParams }: CategoryPageProps) => {
             {/* add mobile filters */}
             <div className="hidden lg:block">
               <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+              <Filter valueKey="colorId" name="Colors" data={colors} />
+            </div>
+            <div className="mt-6 lg:col-span-4 lg:mt-0">
+              {products.length === 0 && <NoResults />}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                {products.map((item) => (
+                  <ProductCard key={item.id} data={item} />
+                ))}
+              </div>
             </div>
           </div>
         </div>
