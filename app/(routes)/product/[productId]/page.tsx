3b728d1b8abdb2d8e4a0e42ac30471110f01ddea
Ron Almog <ron8132@gmail.com>
2023-11-07 09:58:58 -0500

    single product page, images and info

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -2,6 +2,7 @@ import getProduct from "@/actions/get-product";
 import getProducts from "@/actions/get-products";
 import Container from "@/components/container";
 import Gallery from "@/components/gallery";
+import Info from "@/components/info";
 import ProductList from "@/components/product-list";
 import React from "react";
 
@@ -13,7 +14,6 @@ interface ProductPageProps {
 
 const ProductPage = async ({ params }: ProductPageProps) => {
   const product = await getProduct(params.productId);
-  console.log(product);
   const suggestedProducts = await getProducts({
     categoryId: product?.category?.id,
   });
@@ -23,7 +23,9 @@ const ProductPage = async ({ params }: ProductPageProps) => {
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
             <Gallery images={product.images} />
-            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">info</div>
+            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+              <Info data={product} />
+            </div>
           </div>
           <hr className="my-10" />
           <ProductList title="Related Items" items={suggestedProducts} />
