6fef47a6ceb36e2c983c6e884076799dfb060e20
Ron Almog <ron8132@gmail.com>
2023-11-12 17:22:13 -0500

    finish cart item

--- app/(routes)/cart/_components/cart-item.tsx
+++ app/(routes)/cart/_components/cart-item.tsx
@@ -13,6 +13,10 @@ interface CartItemProps {
 }
 
 const CartItem = ({ data }: CartItemProps) => {
+  const cart = useCart();
+  const removeItem = () => {
+    cart.removeItem(data.id);
+  };
   return (
     <li className="flex py-6 border-b">
       <div className="relative h-24 w-24 rounded-md overflow-hidden sm:h-48 sm:w-48">
@@ -23,11 +27,23 @@ const CartItem = ({ data }: CartItemProps) => {
           className="object-cover object-center"
         />
       </div>
-      <div className="relative ml-4 flex flex-1 flex-col justify-between sm:ml-6">
-        <div className="abolute z-10 right-0 top-0">
-          <Button size="icon" variant="secondary">
+      <div className="relative ml-4 flex flex-1 flex-col justify-between sm:ml-6 border border-slate-800">
+        <div className="absolute z-10 right-0 top-0">
+          <Button size="icon" variant="secondary" onClick={removeItem}>
             <X size={15} />
           </Button>
+        </div>
+        <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
+          <div className="flex justify-between">
+            <p className="text-lg font-semibold text-black">{data.name}</p>
+          </div>
+          <div className="mt-1 flex text-sm">
+            <p className="text-gray-500">{data.color.name}</p>
+            <p className="text-gray-500 ml-4 border-l border-gray-200 pl-4">
+              {data.size.name}
+            </p>
+          </div>
+          <Currency value={data.price} />
         </div>
       </div>
     </li>
